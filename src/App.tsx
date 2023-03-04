e2d119d6e192f97fe9dcf92b8a645d2c65dc3b43
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-04 15:44:21 +0300

    feat: added increment and decrement fn for btns

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  let [likes, setLikes] = useState(0)
+
+  const incrementHandler = () => {
+    setLikes(prevState=> prevState + 1)
+  }
+  const decrementHandler = () => {
+    setLikes(prevState=> prevState - 1)
+
+  }
   return (
     <div className="App">
-
+  <h1>{likes}</h1>
+      <button onClick={incrementHandler}>increment</button>
+      <button onClick={decrementHandler}>decrement</button>
     </div>
   );
 }
