30b51c93f490e5734f7be6f8be9a75424722d645
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-11 23:26:53 +0300

    refactor: move PostItem.tsx to self component refactor: remove unused variable and handler from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React, {ChangeEvent, useState} from 'react';
+import { PostItem } from './components/PostItem';
 import './styles/App.css'
 
 function App() {
-    let [value, setValue] = useState('Input text')
 
-    const inputOnchangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.currentTarget.value)
-    }
     return (
         <div className="App">
-            <div className="post">
-                <div className="post_content">
-                    <strong>Lorem ipsum.</strong>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque est exercitationem placeat
-                        porro
-                        veritatis.
-                    </div>
-                </div>
-                <div className="post_button">
-                    <button>Delete</button>
-                </div>
-
-            </div>
+            <PostItem/>
         </div>
     );
 }
---
+++ src/components/PostItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const PostItem = () => {
+    return (
+        <div className="post">
+            <div className="post_content">
+                <strong>Lorem ipsum.</strong>
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque est exercitationem placeat
+                    porro
+                    veritatis.
+                </div>
+            </div>
+            <div className="post_button">
+                <button>Delete</button>
+            </div>
+
+        </div>
+    );
+};
+
