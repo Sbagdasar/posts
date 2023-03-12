1b59986081f88a3882ab7a1b7d643ceb16a45dcb
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-12 16:01:33 +0300

    feat: created CustomButton.tsx and CustomButton.module.css

---
+++ src/components/UI/button/CustomButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from "/CustomButton.module.css"
+export const CustomButton = () => {
+    return (
+        <button className={s.cBtn}>
+
+        </button>
+    );
+};
+
