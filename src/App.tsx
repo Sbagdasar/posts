d5d12e74d849ac85d07c0604b6e1183be9620265
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-14 23:19:31 +0300

    feat: searchBar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './styles/App.css'
 import {v1} from "uuid";
 import {PostsList} from "./components/PostsList";
 import {CreateNewPostForm} from "./features/createNewPost/CreateNewPostForm";
 import {CustomSelect} from "./components/UI/select/CustomSelect";
+import {CustomInput} from "./components/UI/input/CustomInput";
 
 export type PostType = {
     id: string,
@@ -12,13 +13,25 @@ export type PostType = {
 }
 
 export function App() {
+    const[searchQuery, setSearchQuery] = useState('')
     const [posts, setPosts] = useState<PostType[]>([
         {id: v1(), title: 'JavaScript', description: 'learn JS'},
         {id: v1(), title: 'Java', description: 'learn Java'},
         {id: v1(), title: 'MSSQL', description: 'learn MSSQL'},
         {id: v1(), title: 'pyton', description: ' pyton'},
     ])
-    const [selectedSort, setSelectedSort] = useState('Sort by')
+    const [selectedSort, setSelectedSort] = useState('')
+    console.log(searchQuery)
+    function getSortedPosts(){
+        if(selectedSort){
+
+            return  [...posts].sort((a,b)=> a[selectedSort as keyof PostType ].localeCompare(b[selectedSort as keyof PostType ]))
+
+        }
+        return posts
+    }
+
+    const sortedPosts = getSortedPosts()
     const addNewPost = (post: { title: string, description: string }) => {
         setPosts([{id: v1(), ...post}, ...posts])
     }
@@ -27,11 +40,14 @@ export function App() {
     }
     const sortPosts = (sortBy: 'title'|'description') => {
         setSelectedSort(sortBy)
-        setPosts([...posts].sort((a,b)=> a[sortBy].localeCompare(b[sortBy])))
+    }
+    const setSearchValue = (value:string) => {
+        setSearchQuery(value)
     }
     return (
         <div className="App">
             <CreateNewPostForm addNewPost={addNewPost}/>
+            <CustomInput placeholder={'Search'} value={searchQuery} onChange={setSearchValue}/>
             <CustomSelect defaultValue={'Sort by'}
                           options={[
                                 { value: 'Sort by', label: 'Sort by', disabled:true },
@@ -41,7 +57,7 @@ export function App() {
                           value={selectedSort}
                           onChange={sortPosts}
             />
-            <PostsList posts={posts}
+            <PostsList posts={sortedPosts}
                        title={'Programming'}
                        removePost={removePost}/>
         </div>
