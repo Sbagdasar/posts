b0ac7e99af254fc6e078c7daf6b3b6422b073b9b
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-15 20:55:03 +0300

    feat: added animation for posts

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Title from "antd/lib/typography/Title";
 import {PostItem} from "./PostItem";
 import {PostType} from "../App";
+import {
+    CSSTransition,
+    TransitionGroup,
+} from 'react-transition-group';
 import s from './PostsList.module.css'
 
 type PostsListPropsType = {
@@ -16,13 +20,17 @@ export const PostsList = ({posts, ...props}: PostsListPropsType) => {
                 posts.length ? <Title style={{textAlign: 'center'}}>{props.title}</Title>
                     : <Title level={2} style={{textAlign: 'center'}}>No posts</Title>
             }
+            <TransitionGroup>
             {
                 posts.map(post => {
                     return (
-                        <PostItem key={post.id} post={post} removePost={props.removePost}/>
+                        <CSSTransition  key={post.id} timeout={500} classNames="postItem">
+                            <PostItem post={post} removePost={props.removePost}/>
+                        </CSSTransition>
                     )
                 })
             }
+            </TransitionGroup>
         </div>
     );
 };
