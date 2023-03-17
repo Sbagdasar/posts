10a03894ccf11aa55fe15de9d9c8577732e718e4
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2023-03-17 22:06:02 +0300

    feat: Comment.tsx added

---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {CommentType} from "../../dll/postsAPI";
+import s from './Comment.module.css'
+type CommentPropsType ={
+    comment:CommentType
+}
+export const Comment = ({comment}: CommentPropsType) => {
+    return (
+        <div className={s.commentContainer}>
+            <h4>{comment.name}</h4>
+            <p>body: {comment.body}</p>
+            <h5>email: {comment.email}</h5>
+        </div>
+    );
+};
+
